3109c532cfdbcb8e556326656b8ead2893d26d4e
Sabrina Helena <sabrinahelenaf@gmail.com>
2022-06-15 14:51:00 -0300

    consertei o loop, adicionando saída

--- exercicio3-conta_corrente/exercicio3-conta_corrente/Program.cs
+++ exercicio3-conta_corrente/exercicio3-conta_corrente/Program.cs
@@ -6,16 +6,16 @@
         {
             ContaCorrente c1 = new ContaCorrente();
             int i = 0;
-  
-                Console.WriteLine("Olá, qual é o seu nome?: ");
-                c1.Nome = Console.ReadLine();
-                while (i < 1)
-                {
-                    Console.WriteLine($"O que você deseja fazer?>\r\n " +
-                    $"1 - Verificar saldo\r\n " +
-                    $"2 - Sacar\r\n " +
-                    $"3 - Depositar\r\n " +
-                    $"4 - Alterar nome. ");
+
+            Console.WriteLine("Olá, qual é o seu nome?: ");
+            c1.Nome = Console.ReadLine();
+            while (i < 1)
+            {
+                Console.WriteLine($"O que você deseja fazer?>\r\n " +
+                $"1 - Verificar saldo\r\n " +
+                $"2 - Sacar\r\n " +
+                $"3 - Depositar\r\n " +
+                $"4 - Alterar nome. ");
                 int opcao = Convert.ToInt32(Console.ReadLine());
 
                 switch (opcao)
@@ -36,9 +36,12 @@
                         Console.WriteLine("Digite um valor válido, entre 1 e 4");
                         break;
                 }
+                Console.WriteLine("Deseja sair do menu? Digite 1 para sim e 0 para não");
+                i = Convert.ToInt32(Console.ReadLine());
             }
-            Console.WriteLine("Deseja sair do menu? Digite 1 para sim e 0 para não");
-            i = Convert.ToInt32(Console.ReadLine());
+
         }
+
+
     }
 }
