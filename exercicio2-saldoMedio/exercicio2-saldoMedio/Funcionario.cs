d58a9ff9c2ce2d03ce3d1bf964a8b07c96507095
Sabrina Helena <sabrinahelenaf@gmail.com>
2022-06-15 14:01:43 -0300

    adicionei meu primeiro projeto

---
+++ exercicio2-saldoMedio/exercicio2-saldoMedio/Funcionario.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exercicio2_saldoMedio
+{
+    internal class Funcionario
+    {
+        //atribuições
+        public string Nome;
+        public double Salario;
+
+
+
+    }
+}
---
+++ exercicio2-saldoMedio/exercicio2-saldoMedio/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace exercicio2_saldoMedio
+{
+    internal class Program
+    {
+   
+        static void Main(string[] args)
+        {
+            Funcionario f1 = new Funcionario();
+            Funcionario f2 = new Funcionario(); ;//criou 2 funcionarios na classe
+
+            Console.WriteLine("Digite o primeiro nome>");
+            f1.Nome = Console.ReadLine();
+            Console.WriteLine("Digite o salário>");
+            f1.Salario = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("---------------------");
+            Console.WriteLine("Digite o segundo nome>");
+            f2.Nome = Console.ReadLine();
+            Console.WriteLine("Digite o salário>");
+            f2.Salario = Convert.ToDouble(Console.ReadLine());
+
+
+            //função de cálculo da média do salário
+            double calcularMediaSalario(double salario1, double salario2)
+            {
+                return (salario1 + salario2) / 2;
+            }
+
+            Console.WriteLine("A media dos salários é: " + calcularMediaSalario(f1.Salario, f2.Salario));
+
+        }
+    }
+}
