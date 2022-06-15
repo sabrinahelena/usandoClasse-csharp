be1242fc256b207bd58e30082da71b97733d1edb
Sabrina Helena <sabrinahelenaf@gmail.com>
2022-06-15 14:43:44 -0300

    Adicionei o exercício 2, referente a uma simulação de conta corrente

---
+++ exercicio3-conta_corrente/exercicio3-conta_corrente/ContaCorrente.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exercicio3_conta_corrente
+{
+    internal class ContaCorrente
+    {
+        public string Nome;
+        public int NumeroDaConta;
+        public double SaldoDaConta;
+        public string NomeAlterado;
+        public double Deposito;
+        public double Saque;
+
+
+        public void alterarNome()
+        {
+            Console.WriteLine("Digite o novo nome: ");
+            NomeAlterado = Console.ReadLine();
+            Nome = NomeAlterado;
+            Console.WriteLine($"Ok, nome alterado para: {Nome}");
+        }
+        public void depositar()
+        {
+            Console.WriteLine("Quanto você deseja depositar? ");
+            Deposito = Convert.ToDouble(Console.ReadLine());
+            SaldoDaConta += Deposito;
+            Console.WriteLine($"Valor atual: {SaldoDaConta}");
+        }
+
+        public void sacar()
+        {
+            Console.WriteLine("Quanto você deseja sacar? ");
+            Saque  = Convert.ToDouble(Console.ReadLine());
+            SaldoDaConta -= Saque;
+            Console.WriteLine($"Valor atual: {SaldoDaConta}");
+        }
+
+        public void status()
+        {
+            Console.WriteLine($"Olá, {Nome}, bem vindo! Você possui {SaldoDaConta} atualmente em seu banco. ");
+        }
+
+    }
+}
---
+++ exercicio3-conta_corrente/exercicio3-conta_corrente/Program.cs
@@ -0,0 +1,44 @@
+﻿namespace exercicio3_conta_corrente
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ContaCorrente c1 = new ContaCorrente();
+            int i = 0;
+  
+                Console.WriteLine("Olá, qual é o seu nome?: ");
+                c1.Nome = Console.ReadLine();
+                while (i < 1)
+                {
+                    Console.WriteLine($"O que você deseja fazer?>\r\n " +
+                    $"1 - Verificar saldo\r\n " +
+                    $"2 - Sacar\r\n " +
+                    $"3 - Depositar\r\n " +
+                    $"4 - Alterar nome. ");
+                int opcao = Convert.ToInt32(Console.ReadLine());
+
+                switch (opcao)
+                {
+                    case 1:
+                        c1.status();
+                        break;
+                    case 2:
+                        c1.sacar();
+                        break;
+                    case 3:
+                        c1.depositar();
+                        break;
+                    case 4:
+                        c1.alterarNome();
+                        break;
+                    default:
+                        Console.WriteLine("Digite um valor válido, entre 1 e 4");
+                        break;
+                }
+            }
+            Console.WriteLine("Deseja sair do menu? Digite 1 para sim e 0 para não");
+            i = Convert.ToInt32(Console.ReadLine());
+        }
+    }
+}
