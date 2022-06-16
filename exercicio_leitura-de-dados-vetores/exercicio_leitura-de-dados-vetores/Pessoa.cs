1bc9acc3aa3d682f7eae85f40825c170cf2c27ce
Sabrina Helena <sabrinahelenaf@gmail.com>
2022-06-16 16:05:21 -0300

    adicionei mais um exercicio

---
+++ exercicio_leitura-de-dados-vetores/exercicio_leitura-de-dados-vetores/Pessoa.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exercicio_classe_leitura_dados
+{
+    internal class Pessoa
+    {
+        public string Nome;
+        public int Quartos;
+        public double PrecoProduto;
+        public string UltimoNome;
+        public int Idade;
+        public double Altura;
+
+        public void status()
+        {
+            Console.WriteLine($"Olá {Nome} {UltimoNome}! Você tem {Idade} anos e {Altura} altura. Você mora em uma casa com " +
+                $"{Quartos} quartos, e possui um produto que custa {PrecoProduto}. ");
+        }
+    }
+}
---
+++ exercicio_leitura-de-dados-vetores/exercicio_leitura-de-dados-vetores/Program.cs
@@ -0,0 +1,32 @@
+﻿/*
+ *Exercício propõe interação com usuário, lendo dados e depois exibindo, mas lendo vários
+ * dados ao mesmo tempo de tipos diferentes.
+ */
+
+using System;
+using System.Globalization;
+
+namespace exercicio_classe_leitura_dados
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Pessoa pessoa = new Pessoa();
+            Console.WriteLine("Digite seu nome: ");
+            pessoa.Nome = Console.ReadLine();
+            Console.WriteLine("Quantos quartos tem na sua casa?: ");
+            pessoa.Quartos = int.Parse(Console.ReadLine());
+            Console.WriteLine("Entre com o preço de um produto: ");
+            pessoa.PrecoProduto = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.WriteLine("Entre com seu último nome, idade e altura (mesma linha separado por espaços simples): ");
+            string[] v = Console.ReadLine().Split(' ');
+            pessoa.UltimoNome = v[0];
+            pessoa.Idade = int.Parse(v[1]);
+            pessoa.Altura = double.Parse(v[2]);
+      
+
+            pessoa.status();
+        }
+    }
+}
