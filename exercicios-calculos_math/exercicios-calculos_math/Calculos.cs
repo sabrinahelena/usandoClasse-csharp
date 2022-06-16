27cefa5de160c4cf691f6f1f87e3691df419b1f5
Sabrina Helena <sabrinahelenaf@gmail.com>
2022-06-16 16:21:23 -0300

    adicionei mais um exercicio usando calculos

---
+++ exercicios-calculos_math/exercicios-calculos_math/Calculos.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exercicios_calculos_math
+{
+    internal class Calculos
+    {
+        public double valorA;
+        public double valorB;
+        public double valorC;
+        public double valorD;
+        public double raio;
+        protected double pi = 3.14159;
+
+        public double areaCirculo(double raio)
+        {
+            return pi * (Math.Pow(raio, 2));
+        }
+
+        public double calcularDiferenca(double valorA, double valorB, double valorC, double valorD)
+        {
+            return ((valorA * valorB) - (valorC * valorD));
+        }
+
+    }
+}
---
+++ exercicios-calculos_math/exercicios-calculos_math/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Globalization;
+
+namespace exercicios_calculos_math
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Calculos circulo = new Calculos();
+            Calculos diferenca = new Calculos();
+            Console.WriteLine("Digite o raio do círculo: ");
+            circulo.raio = (double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture));
+            Console.WriteLine(circulo.areaCirculo(circulo.raio).ToString("F2"), CultureInfo.InvariantCulture);
+
+            Console.WriteLine("---------------------------");
+            
+            Console.WriteLine("Digite o valor de A, B, C e D respectivamente separado por espaço simples: ");
+            string[] valores = Console.ReadLine().Split(' ');
+            diferenca.valorA = double.Parse(valores[0]);
+            diferenca.valorB = double.Parse(valores[1]);
+            diferenca.valorC = double.Parse(valores[2]);
+            diferenca.valorD = double.Parse(valores[3]);
+
+            Console.WriteLine(diferenca.calcularDiferenca(diferenca.valorA, diferenca.valorB, diferenca.valorC, diferenca.valorD));
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
