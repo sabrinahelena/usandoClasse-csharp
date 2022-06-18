70be62d960fcd89ab8a68fa530aa91a9901a5656
Sabrina Helena <sabrinahelenaf@gmail.com>
2022-06-18 20:26:12 -0300

    adicionei código usando encapsulamento, propriedades e auto propriedades

---
+++ exercicio-encapsulamento/exercicio-encapsulamento/Produto.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Globalization;
+
+namespace exercicio_encapsulamento
+{
+    internal class Produto
+    {
+        private string _nome;
+        private double _preco;
+        //Usando auto propriedade
+        public int Quantidade { get; private set; }
+
+        public Produto()
+        {
+        }
+        public Produto(string nome, double preco, int quantidade)
+        {
+            _nome = nome;
+            _preco = preco;
+            Quantidade = quantidade;
+        }
+
+        //Usando propriedades
+        public string Nome
+        {
+            get { return _nome;  }
+            set
+            {
+                if (value != null && value.Length > 1)
+                {
+                    _nome = value;
+                }
+            }
+        }
+
+        //Modo atributos privados
+        public double GetPreco()
+        {
+            return _preco;
+        }
+
+        public double ValorTotalEmEstoque()
+        {
+            return _preco * Quantidade;
+        }
+        public void AdicionarProdutos(int quantidade)
+        {
+            Quantidade += quantidade;
+        }
+        public void RemoverProdutos(int quantidade)
+        {
+            Quantidade -= quantidade;
+        }
+        public override string ToString()
+        {
+            return _nome
+            + ", $ "
+            + _preco.ToString("F2", CultureInfo.InvariantCulture)
+            + ", "
+            + Quantidade
+            + " unidades, Total: $ "
+            + ValorTotalEmEstoque().ToString("F2", CultureInfo.InvariantCulture);
+        }
+    }
+}
---
+++ exercicio-encapsulamento/exercicio-encapsulamento/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace exercicio_encapsulamento
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Produto p = new Produto("TV", 500.00, 10);
+
+            p.Nome = "TV Asus";
+            Console.WriteLine(p);
+        }
+    }
+}
