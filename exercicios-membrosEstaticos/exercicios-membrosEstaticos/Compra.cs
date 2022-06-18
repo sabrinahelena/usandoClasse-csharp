6546966b765352689e6e3c5098d6ebde810360fd
Sabrina Helena <sabrinahelenaf@gmail.com>
2022-06-18 15:45:18 -0300

    adicionei exercício usando membros estáticos

---
+++ exercicios-membrosEstaticos/exercicios-membrosEstaticos/Compra.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exercicios_membrosEstaticos
+{
+    internal class Compra
+    {
+        public static double imposto = 6.0;
+
+        public static double valorASerPago(double valorDolar, double valorDesejadoCompra)
+        {
+            double resultado = valorDolar * valorDesejadoCompra * (1 + (imposto/100));
+            return resultado;
+        }
+    }
+}
---
+++ exercicios-membrosEstaticos/exercicios-membrosEstaticos/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Globalization;
+
+namespace exercicios_membrosEstaticos
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Cotação dólar: ");
+            double cotacaoDolar = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.Write("Quantos dólares você vai comprar?: ");
+            double valorDolarDesejado = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            double resultadoFinal = Compra.valorASerPago(cotacaoDolar, valorDolarDesejado);
+            Console.WriteLine($"Valor a ser pago em reais: {(resultadoFinal.ToString("F2"),CultureInfo.InvariantCulture)}");
+
+
+        }
+    }
+}
