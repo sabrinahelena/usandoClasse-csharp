7a1bfa7acdf26035b0801eae1755443477854e74
Sabrina Helena <sabrinahelenaf@gmail.com>
2022-06-17 21:06:24 -0300

    adicionei mais um exercício

---
+++ exercicio-calculo_salario_funcionario/exercicio-calculo_salario_funcionario/Funcionario.cs
@@ -0,0 +1,17 @@
+﻿namespace exercicio_calculo_salario_funcionario
+{
+    internal class Funcionario
+    {
+        public int numeroFuncionario;
+        public double horasTrabalhadas;
+        public double valorPorHoras;
+
+
+        public double calcularSalario(double horasTrabalhadas, double valorPorHoras)
+        {
+            return valorPorHoras * horasTrabalhadas;
+        }
+
+       
+    }
+}
---
+++ exercicio-calculo_salario_funcionario/exercicio-calculo_salario_funcionario/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Globalization;
+
+namespace exercicio_calculo_salario_funcionario
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Funcionario f1 = new Funcionario();
+            Console.WriteLine("Digite o seu ID de funcionário: ");
+            f1.numeroFuncionario = (int.Parse(Console.ReadLine()));
+
+            Console.WriteLine("Digite o valor de horas que você trabalho e quanto você ganha por hora: ");
+            string[] dadosSalario = (Console.ReadLine().Split(' '));
+
+            f1.horasTrabalhadas = (double.Parse(dadosSalario[0], CultureInfo.InvariantCulture));
+            f1.valorPorHoras = (double.Parse(dadosSalario[1], CultureInfo.InvariantCulture));
+
+            Console.WriteLine($"O seu salário é de: {f1.calcularSalario(f1.horasTrabalhadas, f1.valorPorHoras).ToString("F2")}");
+
+        }
+    }
+}
