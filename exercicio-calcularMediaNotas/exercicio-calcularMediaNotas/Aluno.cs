62769f858d097fc53a32f3719900b2c7b851d70d
Sabrina Helena <sabrinahelenaf@gmail.com>
2022-06-18 15:24:28 -0300

    adicionei mais um exercício

---
+++ exercicio-calcularMediaNotas/exercicio-calcularMediaNotas/Aluno.cs
@@ -0,0 +1,33 @@
+﻿
+
+namespace exercicio_calcularMediaNotas
+{
+    internal class Aluno
+    {
+        public string nomeAluno;
+        public double nota1;
+        public double nota2;
+        public double nota3;
+        public double resultadoMedia;
+
+        public void calcularNota()
+        {
+            this.resultadoMedia = (nota1 + nota2 + nota3);
+            Console.WriteLine($"Resultado final: {this.resultadoMedia}");
+        }
+
+        public void verificaAprovacao()
+        {
+            if (this.resultadoMedia < 60)
+            {
+                Console.WriteLine($"Reprovado, faltaram {60-this.resultadoMedia} pontos para passar");
+            }
+            else
+            {
+                Console.WriteLine("Aprovado");
+            }
+           
+        }
+
+    }
+}
---
+++ exercicio-calcularMediaNotas/exercicio-calcularMediaNotas/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Globalization;
+
+namespace exercicio_calcularMediaNotas
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Aluno aluno1 = new Aluno();
+            Console.WriteLine("Olá! Digite suas três notas: ");
+            Console.Write("Nota 1: ");
+            aluno1.nota1 = double.Parse(Console.ReadLine());
+            Console.Write("Nota 2: ");
+            aluno1.nota2 = double.Parse(Console.ReadLine());
+            Console.Write("Nota 3: ");
+            aluno1.nota3 = double.Parse(Console.ReadLine());
+
+            if (aluno1.nota1 <= 30 && aluno1.nota2 <= 35 && aluno1.nota3 <= 35)
+            {
+                aluno1.calcularNota();
+                aluno1.verificaAprovacao();
+            }
+            else
+            {
+                Console.WriteLine("Digite notas válidas, o primeiro trimestre vale 30 e o segundo e terceiro valem 35");
+            }
+
+
+        }
+    }
+}
