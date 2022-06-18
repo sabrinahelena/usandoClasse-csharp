33952a03435080672edb15a17a98bda942de76b4
Sabrina Helena <sabrinahelenaf@gmail.com>
2022-06-17 21:27:54 -0300

    adicionei mais um exercício

---
+++ exercicio-lojinha_roupas/exercicio-lojinha_roupas/Loja.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exercicio_lojinha_roupas
+{
+    internal class Loja
+    {
+        public int codigoPeca;
+        public double precoPeca;
+        public int numeroPecas;
+
+        public double valorASerPago(double precoPeca, int numeroPecas)
+        {
+            return numeroPecas * precoPeca;
+        }
+    }
+}
---
+++ exercicio-lojinha_roupas/exercicio-lojinha_roupas/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Globalization;
+
+namespace 
+    
+    exercicio_lojinha_roupas
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Loja cliente = new Loja();
+            Console.WriteLine("Digite o código da peça que deseja comprar, o valor unitário e a quantidade de peças. ");
+            string [] produto = Console.ReadLine().Split(' ');
+
+            cliente.codigoPeca = (int.Parse(produto[0], CultureInfo.InvariantCulture));
+            cliente.precoPeca = (double.Parse(produto[1], CultureInfo.InvariantCulture));
+            cliente.numeroPecas = (int.Parse(produto[2], CultureInfo.InvariantCulture));
+
+            Console.WriteLine($"Você está comprando o produto de código {cliente.codigoPeca}, e o valor a pagar é de {(cliente.valorASerPago(cliente.precoPeca, cliente.numeroPecas).ToString("F2"), CultureInfo.InvariantCulture)}");
+
+            
+        }
+    }
+}
