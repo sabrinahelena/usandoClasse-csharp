7d169ff1ae8df62a6439a8e48cbb41036f8b2786
Sabrina Helena <sabrinahelenaf@gmail.com>
2022-06-19 12:03:06 -0300

    adicionei nova pasta para projetos usando struct

---
+++ usandoStruct/usandoNullable/usandoNullable/Program.cs
@@ -0,0 +1,27 @@
+﻿/* 
+ * O nullable é usado para dados e parâmetros opcionais em struct
+ * 
+ */
+
+namespace usandoNullable
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Nullable<double> x = null; //pode fazer assim
+            double? y = null;//ou assim
+            double? z = 10.0;
+
+            Console.WriteLine(x.GetValueOrDefault()); //pega o valor X ou o valor default que é 0 
+            Console.WriteLine(z.GetValueOrDefault()); //pega o valor X ou o valor default do nulo que é 10 
+
+
+            double a = x ?? 1; //joga o valor de X em A mas se for nulo, joga 1
+            double b = z ?? 2; //joga o valor de X em A mas se for nulo, joga 2
+
+            Console.WriteLine(a);
+            Console.WriteLine(b);
+        }
+    }
+}
---
+++ usandoStruct/usandoStruct/usandoStruct/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace usandoStruct
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Point p; //Pode instanciar assim
+            p.x = 10;
+            p.y = 20;
+
+            Console.WriteLine(p);
+
+
+            p = new Point(); // ou assim
+
+            Console.WriteLine(p);
+        }
+    }
+}
