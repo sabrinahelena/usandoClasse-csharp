d9775396dead152c0c7b5d0fd9df40ef678692e2
Sabrina Helena <sabrinahelenaf@gmail.com>
2022-06-18 21:00:19 -0300

    adicionei exercício onde usei constructor, auto propriedade e propriedade

---
+++ exercicio-Encapsulamento_Propriedades/exercicio-Encapsulamento_Propriedades/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Globalization;
+
+/* 
+ * Exercício:
+ * O programa deve ler o nome de um vendedor, o seu salário fixo e o total de vendas efetuadas
+ * por ele no mês em reais. O vendedor ganha 15% de comissão sob suas vendas 
+ * Informar o valor final que ele receberá.
+ */
+
+
+namespace exercicio_Encapsulamento_Propriedades
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Olá! Seja bem vindo vendedor.");
+            Console.Write("Digite seu nome: ");
+            string nomeVendedor = Console.ReadLine();
+            Console.Write("Digite o total de vendas em reais feita nesse mês: ");
+            double valorVendas = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Vendedor v1 = new Vendedor(nomeVendedor, valorVendas); //usando constructor, coloquei os valores armazenados nas variáveis criadas na classe.
+
+            v1.SalarioFixoPorMes = 1700.00;  //setei o salário fixo por mês.
+            double salarioFinal = v1.calcularSalarioFinal(valorVendas);
+            Console.WriteLine($"Olá {nomeVendedor}, o seu salário final é R$ {salarioFinal.ToString("F2", CultureInfo.InvariantCulture)}");
+
+
+
+        }
+    }
+}
---
+++ exercicio-Encapsulamento_Propriedades/exercicio-Encapsulamento_Propriedades/Vendedor.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exercicio_Encapsulamento_Propriedades
+{
+    internal class Vendedor
+    {
+        public string NomeVendedor;
+        private double _SalarioFixoPorMes;
+        public double ComissaoEmPorcentagem { get; private set; }
+        public double ValorVendasFinal;
+
+        public Vendedor(string nomeVendedor, double valorVendasFinal)
+        {
+            NomeVendedor = nomeVendedor;
+            ValorVendasFinal = valorVendasFinal;
+        }
+
+
+
+        public double SalarioFixoPorMes
+        {
+            get { return _SalarioFixoPorMes; }
+            set { _SalarioFixoPorMes = value; }
+        }
+
+        public double calcularSalarioFinal(double ValorVendasFinal)
+        {
+            ComissaoEmPorcentagem = 15.00;
+            return this.SalarioFixoPorMes + (ValorVendasFinal * (ComissaoEmPorcentagem / 100));
+        }
+
+
+
+
+
+    }
+}
